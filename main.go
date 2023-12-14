25bf00d346236b89e17e265119f88dde4f909cd5
Asaf Shen <asaf@descope.com>
2023-12-14 16:09:19 +0200

    support npm node modules

--- main.go
+++ main.go
@@ -11,12 +11,14 @@ import (
 func main() {
 	tmpGoDir := flag.String("go-project", "", "project directory")
 	tmpNpmDir := flag.String("npm-project", "", "npm directory")
+	// For some project - the node modules are not in the same directory as the package.json
+	tmpNodeModulesDir := flag.String("npm-node-modules", "", "node_modules directory (optional, leave empty if it is in the same as npm-project)")
 	out := flag.String("out", licensecollector.LicenseFileName, "output file")
 	format := flag.String("format", licensecollector.DefaultLicenseFileFormat, "output format: text vs json")
 	flag.Parse()
 	log.SetFlags(0)
 
-	err := licensecollector.Collect(*tmpGoDir, *tmpNpmDir, *out, *format)
+	err := licensecollector.Collect(*tmpGoDir, *tmpNpmDir, *tmpNodeModulesDir, *out, *format)
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
